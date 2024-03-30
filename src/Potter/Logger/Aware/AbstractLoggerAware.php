609e1c8d58e89365f5980aa397117f4d6ab64eb9
Jay Potter <j@ypotter.ca>
2024-03-30 11:12:29 -0400

    remove protected methods

--- src/Potter/Logger/Aware/AbstractLoggerAware.php
+++ src/Potter/Logger/Aware/AbstractLoggerAware.php
@@ -10,7 +10,4 @@ abstract class AbstractLoggerAware implements LoggerAwareInterface
 {
     abstract public function getLogger(): LoggerInterface;
     abstract public function hasLogger(): bool;
-    abstract protected function setLogger(LoggerInterface $logger): void;
-    abstract protected function withLogger(LoggerInterface $logger): LoggerAwareInterface;
-    abstract protected function withoutLogger(): LoggerAwareInterface;
 }
